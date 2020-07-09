96ea5b064f58a14681ba86a7051cc7cbec714203
Marcellus Nwankwo <cellusnwankwo@gmail.com>
2020-07-10 00:25:41 +0100

    Encryption key generated

--- config/services.php
+++ config/services.php
@@ -31,9 +31,9 @@ return [
     ],
 
     'passport' => [
-        'login_endpoint' => env('PASSPORT_LOGIN_ENDPOINT'),
-        'client_id' => env('PASSPORT_CLIENT_ID'),
-        'client_secret' => env('PASSPORT_CLIENT_SECRET'),
+        'login_endpoint' => getenv('PASSPORT_LOGIN_ENDPOINT'),
+        'client_id' => getenv('CLIENT_ID'),
+        'client_secret' => getenv('CLIENT_SECRET'),
     ],
 
 ];
