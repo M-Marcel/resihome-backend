9c244dac01d4b98dcb3404942b76db1f387d0cc0
Marcellus Nwankwo <cellusnwankwo@gmail.com>
2020-07-10 09:37:35 +0100

    Modify users table

---
+++ database/migrations/2020_07_10_081638_modification_to_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModificationToUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('user_role_id');
+            $table->string('phone_number')->nullable();
+            $table->string('postal_code')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
