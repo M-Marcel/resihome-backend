61bd7f781797c965464cc25ed2736a6585a26dc7
Marcellus Nwankwo <cellusnwankwo@gmail.com>
2020-07-12 14:24:21 +0100

    Removed required from userRole

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -53,7 +53,7 @@ class AuthController extends Controller
         $request->validate([
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
-            'userRole' => 'required|integer',
+            'userRole' => 'integer',
             'firstName' => 'string',
             'lastName' => 'string',
             'postalCode' => 'string',
