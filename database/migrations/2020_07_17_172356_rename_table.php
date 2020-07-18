f128727a3edc11f61875074b4c57496a8810698e
Marcellus Nwankwo <cellusnwankwo@gmail.com>
2020-07-18 14:08:56 +0100

    remote register fix

--- database/migrations/2020_07_17_172356_rename_table.php
+++ database/migrations/2020_07_17_172356_rename_table.php
@@ -13,25 +13,25 @@ class RenameTable extends Migration
      */
     public function up()
     {
-        // Schema::rename('tbl-admin', 'admin');
-        // Schema::rename('tbl_agent', 'agent');
-        // Schema::rename('tbl_building', 'building');
-        // Schema::rename('tbl_agent_location', 'agent_location');
-        // Schema::rename('tbl_location', 'location');
-        // Schema::rename('tbl_community', 'community');
-        // Schema::rename('tbl_community_image', 'community_image');
-        // Schema::rename('tbl_history', 'history');
-        // Schema::rename('tbl_home_builder', 'home_builder');
-        // Schema::rename('tbl_lawyer', 'lawyer');
-        // Schema::rename('tbl_mansion', 'mansion');
-        // Schema::rename('tbl_mansion_image', 'mansion_image');
-        // Schema::rename('tbl_mortgage-broker', 'mortgage_broker');
-        // Schema::rename('tbl_property', 'property');
-        // Schema::rename('tbl_property_image', 'property_image');
-        // Schema::rename('tbl_property_inspector', 'property_inspector');
-        // Schema::rename('tbl_property_school', 'school');
-        // Schema::rename('tbl_region', 'region');
-        // Schema::rename('tbl_review', 'review');
+        Schema::rename('tbl-admin', 'admin');
+        Schema::rename('tbl_agent', 'agent');
+        Schema::rename('tbl_building', 'building');
+        Schema::rename('tbl_agent_location', 'agent_location');
+        Schema::rename('tbl_location', 'location');
+        Schema::rename('tbl_community', 'community');
+        Schema::rename('tbl_community_image', 'community_image');
+        Schema::rename('tbl_history', 'history');
+        Schema::rename('tbl_home_builder', 'home_builder');
+        Schema::rename('tbl_lawyer', 'lawyer');
+        Schema::rename('tbl_mansion', 'mansion');
+        Schema::rename('tbl_mansion_image', 'mansion_image');
+        Schema::rename('tbl_mortgage-broker', 'mortgage_broker');
+        Schema::rename('tbl_property', 'property');
+        Schema::rename('tbl_property_image', 'property_image');
+        Schema::rename('tbl_property_inspector', 'property_inspector');
+        Schema::rename('tbl_property_school', 'school');
+        Schema::rename('tbl_region', 'region');
+        Schema::rename('tbl_review', 'review');
         Schema::rename('tbl_school', 'schools');
     }
 
