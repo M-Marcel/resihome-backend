55f6c4740b26939606436e341dcc0c41ef3bc764
Marcellus Nwankwo <cellusnwankwo@gmail.com>
2020-08-07 17:16:55 +0100

    admin registration fixed

--- app/Mansion.php
+++ app/Mansion.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mansion extends Model
 {
-    //
+    protected $table = 'mansion';
+
+    protected $guarded = [];
+
 }
--- routes/api.php
+++ routes/api.php
@@ -80,6 +80,7 @@ Route::get('/propertyshort', 'Property\ShortLeasePropertyController@index');
 
 Route::get('/property/{id}', 'PropertyController@show');
 Route::get('/allMansion', 'MansionController@index');
+Route::get('/showMansion', 'MansionController@show');
 Route::get('/search', 'PropertyController@search');
 Route::get('/sold', 'PropertyController@sold');
 Route::get('/isSold/{id}', 'PropertyController@isSold');
