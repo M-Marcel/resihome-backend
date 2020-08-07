84843a113104e573169152c2d519f920656d83ab
Marcellus Nwankwo <cellusnwankwo@gmail.com>
2020-08-07 17:56:03 +0100

    mansion edit

---
+++ database/migrations/2020_08_07_165207_add_owner_id_to_mansion.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddOwnerIdToMansion extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mansion', function (Blueprint $table) {
+            $table->foreignId('owner_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mansion', function (Blueprint $table) {
+            //
+        });
+    }
+}
