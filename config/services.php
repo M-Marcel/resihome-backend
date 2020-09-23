c218d14af64a3117b14fadab22d0c5fdc1ee226f
Marcellus Nwankwo <cellusnwankwo@gmail.com>
2020-09-23 23:32:51 +0100

    search address

--- config/services.php
+++ config/services.php
@@ -25,50 +25,50 @@ return [
     ],
 
     'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+        // 'key' => env('AWS_ACCESS_KEY_ID'),
+        // 'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        // 'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
 
-        // 'key' => getenv('AWS_ACCESS_KEY_ID'),
-        // 'secret' => getenv('AWS_SECRET_ACCESS_KEY'),
-        // 'region' => getenv('AWS_DEFAULT_REGION', 'us-east-2'),
-        // 'bucket' => getenv('AWS_BUCKET'),
+        'key' => getenv('AWS_ACCESS_KEY_ID'),
+        'secret' => getenv('AWS_SECRET_ACCESS_KEY'),
+        'region' => getenv('AWS_DEFAULT_REGION', 'us-east-2'),
+        'bucket' => getenv('AWS_BUCKET'),
     ],
 
     'passport' => [
-        // 'login_endpoint' => getenv('PASSPORT_LOGIN_ENDPOINT'),
-        // 'client_id' => getenv('CLIENT_ID'),
-        // 'client_secret' => getenv('CLIENT_SECRET'),
-        'login_endpoint' => env('PASSPORT_LOGIN_ENDPOINT'),
-        'client_id' => env('PASSPORT_CLIENT_ID'),
-        'client_secret' => env('PASSPORT_CLIENT_SECRET'),
+        'login_endpoint' => getenv('PASSPORT_LOGIN_ENDPOINT'),
+        'client_id' => getenv('CLIENT_ID'),
+        'client_secret' => getenv('CLIENT_SECRET'),
+        // 'login_endpoint' => env('PASSPORT_LOGIN_ENDPOINT'),
+        // 'client_id' => env('PASSPORT_CLIENT_ID'),
+        // 'client_secret' => env('PASSPORT_CLIENT_SECRET'),
     ],
 
     'google' => [
-        'client_id' => env('GOOGLE_CLIENT_ID'),
-        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('GOOGLE_REDIRECT_URI'),
-        // 'client_id' => getenv('GOOGLE_CLIENT_ID'),
-        // 'client_secret' => getenv('GOOGLE_CLIENT_SECRET'),
-        // 'redirect' => getenv('GOOGLE_REDIRECT_URI')
+        // 'client_id' => env('GOOGLE_CLIENT_ID'),
+        // 'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        // 'redirect' => env('GOOGLE_REDIRECT_URI'),
+        'client_id' => getenv('GOOGLE_CLIENT_ID'),
+        'client_secret' => getenv('GOOGLE_CLIENT_SECRET'),
+        'redirect' => getenv('GOOGLE_REDIRECT_URI')
     ],
 
     'facebook' => [
-        'client_id' => env('FACEBOOK_CLIENT_ID'),
-        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => env('FACEBOOK_REDIRECT_URI')
-        // 'client_id' => getenv('FACEBOOK_CLIENT_ID'),
-        // 'client_secret' => getenv('FACEBOOK_CLIENT_SECRET'),
-        // 'redirect' => getenv('FACEBOOK_REDIRECT_URI')
+        // 'client_id' => env('FACEBOOK_CLIENT_ID'),
+        // 'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        // 'redirect' => env('FACEBOOK_REDIRECT_URI')
+        'client_id' => getenv('FACEBOOK_CLIENT_ID'),
+        'client_secret' => getenv('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => getenv('FACEBOOK_REDIRECT_URI')
       ],
 
       'apple' => [
-        'client_id' => env('APPLE_CLIENT_ID'),
-        'client_secret' => env('APPLE_CLIENT_SECRET'),
-        'redirect' => env('APPLE_REDIRECT_URI')
-        // 'client_id' => getenv('APPLE_CLIENT_ID'),
-        // 'client_secret' => getenv('APPLE_CLIENT_SECRET'),
-        // 'redirect' => getenv('APPLE_REDIRECT_URI')
+        // 'client_id' => env('APPLE_CLIENT_ID'),
+        // 'client_secret' => env('APPLE_CLIENT_SECRET'),
+        // 'redirect' => env('APPLE_REDIRECT_URI')
+        'client_id' => getenv('APPLE_CLIENT_ID'),
+        'client_secret' => getenv('APPLE_CLIENT_SECRET'),
+        'redirect' => getenv('APPLE_REDIRECT_URI')
       ],
 
 ];
